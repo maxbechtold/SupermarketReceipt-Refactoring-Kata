09ed99c8b5cac7df259b81f86f46219d8a6db08e
Max Bechtold <max.bechtold@andrena.de>
2020-01-30 17:10:26 +0100

    Using seed to create random tests

--- java/src/test/java/dojo/supermarket/model/FakeCatalog.java
+++ java/src/test/java/dojo/supermarket/model/FakeCatalog.java
@@ -1,7 +1,10 @@
 package dojo.supermarket.model;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.stream.Stream;
 
 public class FakeCatalog implements SupermarketCatalog {
     private Map<String, Product> products = new HashMap<>();
@@ -16,5 +19,11 @@ public class FakeCatalog implements SupermarketCatalog {
     @Override
     public double getUnitPrice(Product p) {
         return this.prices.get(p.getName());
+    }
+    
+    public Product getRandomProduct(Random random ) {
+        Collection<Product> products = this.products.values();
+        Stream<Product> selectedStream = products.stream().skip(random.nextInt(products.size()));
+        return selectedStream.findFirst().get();
     }
 }
