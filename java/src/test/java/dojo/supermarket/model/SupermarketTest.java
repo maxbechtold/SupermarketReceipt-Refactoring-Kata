439d3ba624a5c2de89e1b252bef68c99af9452f3
Max Bechtold <max.bechtold@andrena.de>
2020-01-30 16:53:00 +0100

    Rewrote SupermarketTest using maxbechtold/golden-master

--- java/src/test/java/dojo/supermarket/model/SupermarketTest.java
+++ java/src/test/java/dojo/supermarket/model/SupermarketTest.java
@@ -1,11 +1,21 @@
 package dojo.supermarket.model;
 
 import dojo.supermarket.ReceiptPrinter;
-import org.approvaltests.Approvals;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import maxbe.goldenmaster.junit.extension.GoldenMasterRun;
+import maxbe.goldenmaster.junit.extension.GoldenMasterTest;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+
+@GoldenMasterTest
 public class SupermarketTest {
+
+    private FileWriter fileWriter;
+    private ReceiptPrinter receiptPrinter = new ReceiptPrinter(40);
+
     private SupermarketCatalog catalog;
     private Teller teller;
     private ShoppingCart theCart;
@@ -15,7 +25,9 @@ public class SupermarketTest {
     private Product cherryTomatoes;
 
     @BeforeEach
-    public void setUp() {
+    void setUp(File output, Integer index) throws IOException {
+        fileWriter = new FileWriter(output);
+        
         catalog = new FakeCatalog();
         teller = new Teller(catalog);
         theCart = new ShoppingCart();
@@ -28,49 +40,53 @@ public class SupermarketTest {
         catalog.addProduct(apples, 1.99);
         cherryTomatoes = new Product("cherry tomato box", ProductUnit.Each);
         catalog.addProduct(cherryTomatoes, 0.69);
-
     }
 
-    @Test
-    public void an_empty_shopping_cart_should_cost_nothing() {
+    @GoldenMasterRun(repetitions = 1)
+    void an_empty_shopping_cart_should_cost_nothing() {
         Receipt receipt = teller.checksOutArticlesFrom(theCart);
-        Approvals.verify(new ReceiptPrinter(40).printReceipt(receipt));
+        
+        writeOut(receiptPrinter.printReceipt(receipt));
     }
 
-    @Test
-    public void one_normal_item() {
+    @GoldenMasterRun(repetitions = 1)
+    void one_normal_item() {
         theCart.addItem(toothbrush);
         Receipt receipt = teller.checksOutArticlesFrom(theCart);
-        Approvals.verify(new ReceiptPrinter(40).printReceipt(receipt));
+        
+        writeOut(receiptPrinter.printReceipt(receipt));
     }
 
-    @Test
-    public void two_normal_items() {
+    @GoldenMasterRun(repetitions = 1)
+    void two_normal_items() {
         theCart.addItem(toothbrush);
         theCart.addItem(rice);
         Receipt receipt = teller.checksOutArticlesFrom(theCart);
-        Approvals.verify(new ReceiptPrinter(40).printReceipt(receipt));
+
+        writeOut(receiptPrinter.printReceipt(receipt));
     }
 
-    @Test
-    public void buy_two_get_one_free() {
+    @GoldenMasterRun(repetitions = 1)
+    void buy_two_get_one_free() {
         theCart.addItem(toothbrush);
         theCart.addItem(toothbrush);
         theCart.addItem(toothbrush);
         teller.addSpecialOffer(SpecialOfferType.ThreeForTwo, toothbrush, catalog.getUnitPrice(toothbrush));
         Receipt receipt = teller.checksOutArticlesFrom(theCart);
-        Approvals.verify(new ReceiptPrinter(40).printReceipt(receipt));
+        
+        writeOut(receiptPrinter.printReceipt(receipt));
     }
 
-    @Test
-    public void buy_two_get_one_free_but_insufficient_in_basket() {
+    @GoldenMasterRun(repetitions = 1)
+    void buy_two_get_one_free_but_insufficient_in_basket() {
         theCart.addItem(toothbrush);
         teller.addSpecialOffer(SpecialOfferType.ThreeForTwo, toothbrush, catalog.getUnitPrice(toothbrush));
         Receipt receipt = teller.checksOutArticlesFrom(theCart);
-        Approvals.verify(new ReceiptPrinter(40).printReceipt(receipt));
+        
+        writeOut(receiptPrinter.printReceipt(receipt));
     }
-    @Test
-    public void buy_five_get_one_free() {
+    @GoldenMasterRun(repetitions = 1)
+    void buy_five_get_one_free() {
         theCart.addItem(toothbrush);
         theCart.addItem(toothbrush);
         theCart.addItem(toothbrush);
@@ -78,70 +94,89 @@ public class SupermarketTest {
         theCart.addItem(toothbrush);
         teller.addSpecialOffer(SpecialOfferType.ThreeForTwo, toothbrush, catalog.getUnitPrice(toothbrush));
         Receipt receipt = teller.checksOutArticlesFrom(theCart);
-        Approvals.verify(new ReceiptPrinter(40).printReceipt(receipt));
+        
+        writeOut(receiptPrinter.printReceipt(receipt));
     }
 
-    @Test
-    public void loose_weight_product() {
+    @GoldenMasterRun(repetitions = 1)
+    void loose_weight_product() {
         theCart.addItemQuantity(apples, .5);
         Receipt receipt = teller.checksOutArticlesFrom(theCart);
-        Approvals.verify(new ReceiptPrinter(40).printReceipt(receipt));
+        
+        writeOut(receiptPrinter.printReceipt(receipt));
     }
 
-    @Test
-    public void percent_discount() {
+    @GoldenMasterRun(repetitions = 1)
+    void percent_discount() {
         theCart.addItem(rice);
         teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, rice, 10.0);
         Receipt receipt = teller.checksOutArticlesFrom(theCart);
-        Approvals.verify(new ReceiptPrinter(40).printReceipt(receipt));
+        
+        writeOut(receiptPrinter.printReceipt(receipt));
     }
 
-    @Test
-    public void xForY_discount() {
+    @GoldenMasterRun(repetitions = 1)
+    void xForY_discount() {
         theCart.addItem(cherryTomatoes);
         theCart.addItem(cherryTomatoes);
         teller.addSpecialOffer(SpecialOfferType.TwoForAmount, cherryTomatoes,.99);
         Receipt receipt = teller.checksOutArticlesFrom(theCart);
-        Approvals.verify(new ReceiptPrinter(40).printReceipt(receipt));
+        
+        writeOut(receiptPrinter.printReceipt(receipt));
     }
 
-    @Test
-    public void xForY_discount_with_insufficient_in_basket() {
+    @GoldenMasterRun(repetitions = 1)
+    void xForY_discount_with_insufficient_in_basket() {
         theCart.addItem(cherryTomatoes);
         teller.addSpecialOffer(SpecialOfferType.TwoForAmount, cherryTomatoes,.99);
         Receipt receipt = teller.checksOutArticlesFrom(theCart);
-        Approvals.verify(new ReceiptPrinter(40).printReceipt(receipt));
+        
+        writeOut(receiptPrinter.printReceipt(receipt));
     }
 
-    @Test
-    public void FiveForY_discount() {
+    @GoldenMasterRun(repetitions = 1)
+    void FiveForY_discount() {
         theCart.addItemQuantity(apples, 5);
         teller.addSpecialOffer(SpecialOfferType.FiveForAmount, apples,6.99);
         Receipt receipt = teller.checksOutArticlesFrom(theCart);
-        Approvals.verify(new ReceiptPrinter(40).printReceipt(receipt));
+        
+        writeOut(receiptPrinter.printReceipt(receipt));
     }
 
-    @Test
-    public void FiveForY_discount_withSix() {
+    @GoldenMasterRun(repetitions = 1)
+    void FiveForY_discount_withSix() {
         theCart.addItemQuantity(apples, 6);
         teller.addSpecialOffer(SpecialOfferType.FiveForAmount, apples,5.99);
         Receipt receipt = teller.checksOutArticlesFrom(theCart);
-        Approvals.verify(new ReceiptPrinter(40).printReceipt(receipt));
+        
+        writeOut(receiptPrinter.printReceipt(receipt));
     }
 
-    @Test
-    public void FiveForY_discount_withSixteen() {
+    @GoldenMasterRun(repetitions = 1)
+    void FiveForY_discount_withSixteen() {
         theCart.addItemQuantity(apples, 16);
         teller.addSpecialOffer(SpecialOfferType.FiveForAmount, apples,7.99);
         Receipt receipt = teller.checksOutArticlesFrom(theCart);
-        Approvals.verify(new ReceiptPrinter(40).printReceipt(receipt));
+        
+        writeOut(receiptPrinter.printReceipt(receipt));
     }
 
-    @Test
-    public void FiveForY_discount_withFour() {
+    @GoldenMasterRun(repetitions = 1)
+    void FiveForY_discount_withFour() {
         theCart.addItemQuantity(apples, 4);
         teller.addSpecialOffer(SpecialOfferType.FiveForAmount, apples,8.99);
         Receipt receipt = teller.checksOutArticlesFrom(theCart);
-        Approvals.verify(new ReceiptPrinter(40).printReceipt(receipt));
+        
+        writeOut(receiptPrinter.printReceipt(receipt));
     }
+
+    private void writeOut(String receiptPrint) {
+        try {
+            this.fileWriter.append(receiptPrint);
+            this.fileWriter.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
